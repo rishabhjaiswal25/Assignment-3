f350505a8f66b0876dff5aacbb8c76a2508f886f
goutam1999 <87134744+goutam1999@users.noreply.github.com>
2022-04-17 16:02:02 +0530

    Create question3.java

---
+++ Goutam Nandi/question3.java
@@ -0,0 +1,39 @@
+package assignment_3;
+import java.util.*;
+
+public class question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the no of elements:");
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("enter the elements:");
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		
+		System.out.println("sorted elements:");
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
