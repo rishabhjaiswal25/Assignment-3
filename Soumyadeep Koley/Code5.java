6e17e00423394e2445d7b68ba428c91223b58abf
Soumyadeepkoley <66461161+Soumyadeepkoley@users.noreply.github.com>
2022-04-18 14:33:25 +0530

    Update Code5.java

--- Soumyadeep Koley/Code5.java
+++ Soumyadeep Koley/Code5.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Assignment3question5 {
+public class Code5 {
 public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter row size ");
